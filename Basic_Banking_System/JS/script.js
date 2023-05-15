e2511162c47e138cff8caa6ec1157108bdc7bb3e
Ansh Raj Singh <arajsinghjadoun@gmail.com>
2023-05-15 15:30:54 +0530

    deleted script.js

--- Basic_Banking_System/JS/script.js
+++
@@ -1,7 +0,0 @@
-function sendMoney(){
-    var enterName = document.getElementById("enterName").value;
-    var enterAmount = parseInt(document.getElementById("enterAmount").value);
- var enterSName = document.getElementById("enterSName").value;
-  var findSenderBankAccount = enterSName + "BankBalance";
-    var enterSAmount =parseInt(document.getElementById(findSenderBankAccount).innerHTML);
-}
